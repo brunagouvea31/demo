8808bff790273e22d75de1b26fad8d7953e0da9e
Bruna <arq.brunagouvea@gmail.com>
2022-05-30 17:06:00 +0100

    Initial commit

---
+++ src/main/java/repository/PeopleRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.data.repository;
+
+
+import com.example.demo.People;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class PeopleRepository extends JpaRepository<People, Integer> {
+}
